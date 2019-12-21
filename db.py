ddf38f230b45436acda021239e734e94f3a66952
Tim Guggenmos <tim.guggenmos@me.com>
2019-12-21 19:39:56 +0100

    db stats dummy

--- db.py
+++ db.py
@@ -13,3 +13,26 @@ async def get_last_clicks(count: int = 1000):
     """Returns last `count` clicks from the database."""
     # return db.last(count)
     return [{"date": 123456789, "name": "Name", "comment": "Comment"}] * 5
+
+
+async def get_total_clicks():
+    """Returns the total amout of clicks."""
+    return 900
+
+
+async def get_day_clicks():
+    """Returns the amout of clicks today."""
+    return 500
+
+
+async def get_hour_clicks():
+    """Returns the amout of clicks in the last hour."""
+    return 100
+
+
+async def get_stats():
+    return {
+        "total": await get_total_clicks(),
+        "day": await get_day_clicks(),
+        "hour": await get_hour_clicks()
+    }
