676ccca586bf74e766f7774de40929c9046610b6
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-05 08:34:38 +0100

    cache click counts (no db, yet)

--- db.py
+++ db.py
@@ -2,9 +2,17 @@ import sqlite3
 import datetime
 
 
+current_total_clicks = 900
+current_day_clicks = 500
+current_hour_clicks = 100
+
+
 async def add_click(name: str, comment: str):
     """Stores a click to the database and returns this click object (dict)."""
     click = {"date": 123456789, "name": name, "comment": comment}
+    current_total_clicks += 1
+    current_day_clicks += 1
+    current_hour_clicks += 1
     # db.store(click)
     return click
 
@@ -17,20 +25,21 @@ async def get_last_clicks(count: int = 1000):
 
 async def get_total_clicks():
     """Returns the total amout of clicks."""
-    return 900
+    return current_total_clicks
 
 
 async def get_day_clicks():
     """Returns the amout of clicks today."""
-    return 500
+    return current_day_clicks
 
 
 async def get_hour_clicks():
     """Returns the amout of clicks in the last hour."""
-    return 100
+    return current_hour_clicks
 
 
 async def get_stats():
+    # TODO: await together
     return {
         "total": await get_total_clicks(),
         "day": await get_day_clicks(),
