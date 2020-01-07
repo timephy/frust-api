aece4932095ca14741c632f1ac13c48b5c164d38
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-07 20:08:27 +0100

    sio_catch_error + extract_event_date

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from aiohttp import web
 import json
 
+import functools
+
 
 def extract_click_data(data):
     if "name" not in data or type(data["name"]) is not str:
@@ -11,6 +13,22 @@ def extract_click_data(data):
     # return getattr(data, "name"), getattr(data, "comment", None)
 
 
+def extract_event_data(data):
+    if "id" not in data or type(data["id"]) is not str:
+        raise Exception("id must be of type String")
+    return data["id"]
+
+
 def dumps(data):
     """json.dumps with indent of 2"""
     return web.Response(text=json.dumps(data, indent=2))
+
+
+def sio_catch_error(func):
+    @functools.wraps(func)
+    async def wrapper(sid, data):
+        try:
+            return await func(sid, data)
+        except Exception as e:
+            print(f"{sid} sent invalid data (to {func.__name__}):", e)
+    return wrapper
