0f31440999115255c4ad527fc227c03205880383
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-09 22:33:50 +0100

    added list users (name, clicks) feature

--- __main__.py
+++ __main__.py
@@ -17,6 +17,9 @@ sio.attach(app, socketio_path="/api/socket.io")
 
 # A counter for the connected socket.io client
 client_counter = 0
+client_ids = []
+client_names = {}
+client_session_clicks = {}
 
 
 # HTTP Routes
@@ -28,13 +31,30 @@ async def history(request):
     return utils.dumps(await db.get_last_clicks())
 
 
+async def list_current_users(request):
+    return utils.dumps({
+        "user_count": client_counter,
+        "identified_users": [{
+            "name": client_names.get(sid, None),
+            "session_clicks": client_session_clicks.get(sid, 0)
+        } for sid in client_ids]
+    })
+
+
 # Socket.io Events
 @sio.event
 @utils.sio_catch_error
 async def click(sid, data):
     print(f"click({sid}, {data})")
 
+    global client_session_clicks
+    client_session_clicks[sid] += 1
+
     name, comment, style = utils.extract_click_data(data)
+
+    global client_names
+    client_names[sid] = name
+
     click = await db.add_click(name, comment, style)
     await sio.emit("click", click)
 
@@ -45,6 +65,10 @@ async def event(sid, data):
     print(f"event({sid}, {data})")
 
     name, event_id = utils.extract_event_data(data)
+
+    global client_names
+    client_names[sid] = name
+
     await sio.emit("event", {"name": name, "id": event_id})
 
 
@@ -52,8 +76,15 @@ async def event(sid, data):
 async def connect(sid, environ):
     print(f"connect({sid})")  # ", {environ})")
 
+    global client_ids
+    client_ids.append(sid)
+
+    global client_session_clicks
+    client_session_clicks[sid] = 0
+
     global client_counter
     client_counter += 1
+
     await sio.emit("stats", await db.get_stats(), room=sid)
     await sio.emit("users", {"count": client_counter})
 
@@ -61,9 +92,18 @@ async def connect(sid, environ):
 @sio.event
 async def disconnect(sid):
     print(f"disconnect({sid})")
+    global client_ids
+    client_ids.remove(sid)
 
     global client_counter
     client_counter -= 1
+
+    global client_session_clicks
+    del client_session_clicks[sid]
+
+    global client_names
+    del client_names[sid]
+
     await sio.emit("users", {"count": client_counter})
 
 
@@ -74,7 +114,8 @@ async def connect_error():
 # set routes of app
 app.add_routes([
     web.get("/api", index),
-    web.get("/api/history", history)
+    web.get("/api/history", history),
+    web.get("/api/list", list_current_users)
 ])
 
 # serve frontend for development
